c1c250687f18ad8f663f337b9ae27e2768ae97ca
Quinton Ashley <qashto@gmail.com>
2018-08-23 21:39:09 -0400

    0.0.5

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 module.exports = function(name) {
-  const ret = require('path').resolve(`./node_modules/gitignore-templates/gitignore/${name}.gitignore`);
-  return ret;
+  return ret require('path').join(__dirname, `/gitignore/${name}.gitignore`);
 }
